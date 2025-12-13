d0be1a95f256432e4193dc0b743e266c2dfda243
Lukas Korten <mail@lukas-korten.de>
2025-12-13 21:53:35 +0100

    Enhance ticket and homepage UI: add animations, update layout, and improve accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,17 @@ import { ticketsPath } from './paths';
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Home Page</h2>
-      <Link href={ticketsPath()} className="underline text-sm">
-        Tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm ">Your home place to start</p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline text-sm">
+          Tickets
+        </Link>
+      </div>
     </div>
   );
 };
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,20 +1,43 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import { initialTickets } from '@/app/data';
 import { ticketPath } from '@/app/paths';
 
+const TICKET_ICONS = {
+  OPEN: 'O',
+  DONE: 'X',
+  IN_PROGRESS: '>',
+};
+
 const TicketsPage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Tickets Page</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+        <p className="text-sm ">All your tickets at one place</p>
+      </div>
 
-      {initialTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <h3 className="text-lg">{ticket.title}</h3>
-          <Link href={ticketPath(ticket.id)} className="underline text-sm">
-            View
-          </Link>
-        </div>
-      ))}
+      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        {initialTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className="w-full max-w-105 p-4 border border-slate-100 rounded"
+          >
+            <div>{TICKET_ICONS[ticket.status]}</div>
+            <h2 className="text-lg font-semibold truncate">{ticket.title}</h2>
+            <p
+              className={clsx('text-sm text-slate-500 truncate', {
+                'line-through': ticket.status === 'DONE',
+              })}
+            >
+              {ticket.content}
+            </p>
+            <Link href={ticketPath(ticket.id)} className="underline text-sm">
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
