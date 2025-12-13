4ad3caae112f3b1879a82a3f7f36eea81cabecff
Lukas Korten <mail@lukas-korten.de>
2025-12-13 07:05:56 +0100

    add initial tickets data

---
+++ app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,23 @@
+import { initialTickets } from '@/app/data';
+
+type TicketPageProps = {
+  params: Promise<{ ticketId: string }>;
+};
+
+const TicketPage = async ({ params }: TicketPageProps) => {
+  const { ticketId } = await params;
+  const ticket = initialTickets.find(({ id }) => id === ticketId);
+
+  if (!ticket) {
+    return <p>Ticket not found</p>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
+};
+
+export default TicketPage;
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+import { initialTickets } from '../data';
+
 const TicketsPage = () => {
-  return <h2 className="text-lg">Tickets Page</h2>;
+  return (
+    <div>
+      <h2 className="text-lg">Tickets Page</h2>
+
+      {initialTickets.map((ticket) => (
+        <div key={ticket.id}>
+          <h3 className="text-lg">{ticket.title}</h3>
+          <Link href={`/tickets/${ticket.id}`} className="underline text-sm">
+            View
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TicketsPage;
