d025fd3451f57684297e39e5ac32a3c6188e9bcd
Lukas Korten <mail@lukas-korten.de>
2025-12-15 11:51:16 +0100

    feat: implement ticket fetching from API in TicketsPage component

---
+++ src/features/ticket/queries/get-tickets.ts
@@ -0,0 +1,7 @@
+import { initialTickets } from '@/data';
+import { Ticket } from '../types';
+
+export async function getTickets(): Promise<Ticket[]> {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return new Promise((resolve) => resolve(initialTickets));
+}
