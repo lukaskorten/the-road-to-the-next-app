49301485457cde2a159813cb5eb1d361735bf84b
Lukas Korten <mail@lukas-korten.de>
2025-12-15 06:03:38 +0100

    refactor TicketsPage to use Card component for ticket display

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,6 +1,13 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { ticketPath } from '@/app/paths';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { initialTickets } from '@/data';
 
 const CheckIcon = () => (
@@ -70,23 +77,24 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <div
-            key={ticket.id}
-            className="w-full max-w-105 p-4 border border-slate-100 rounded"
-          >
-            <div>{TICKET_ICONS[ticket.status]}</div>
-            <h2 className="text-lg font-semibold truncate">{ticket.title}</h2>
-            <p
-              className={clsx('text-sm text-slate-500 truncate', {
-                'line-through': ticket.status === 'DONE',
-              })}
-            >
-              {ticket.content}
-            </p>
-            <Link href={ticketPath(ticket.id)} className="underline text-sm">
-              View
-            </Link>
-          </div>
+          <Card key={ticket.id} className="w-full max-w-105">
+            <CardHeader>
+              <CardTitle className="flex gap-x-2 items-center">
+                <span>{TICKET_ICONS[ticket.status]}</span>
+                <span className="truncate">{ticket.title}</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="line-clamp-3 whitespace-break-spaces">
+                {ticket.content}
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Link href={ticketPath(ticket.id)} className="underline text-sm">
+                View
+              </Link>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
