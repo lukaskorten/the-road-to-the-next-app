60b0db1a818754d1739836f655c3b7fee082e3ae
Lukas Korten <mail@lukas-korten.de>
2025-12-15 09:50:28 +0100

    refactor: simplify TicketItem component structure and improve button handling

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -1,12 +1,8 @@
+import { LucideSquareArrowOutUpRight } from 'lucide-react';
 import Link from 'next/link';
 import { ticketPath } from '@/app/paths';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { buttonVariants } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { TICKET_ICONS } from '../constants';
 import { Ticket } from '../types';
 
@@ -15,23 +11,32 @@ type TicketItemProps = {
 };
 
 function TicketItem({ ticket }: TicketItemProps) {
+  const detailsButton = (
+    <Link
+      href={ticketPath(ticket.id)}
+      className={buttonVariants({ variant: 'outline', size: 'icon' })}
+    >
+      <LucideSquareArrowOutUpRight />
+    </Link>
+  );
+
   return (
-    <Card className="w-full max-w-105">
-      <CardHeader>
-        <CardTitle className="flex gap-x-2 items-center">
-          <span>{TICKET_ICONS[ticket.status]}</span>
-          <span className="truncate">{ticket.title}</span>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <p className="line-clamp-3 whitespace-break-spaces">{ticket.content}</p>
-      </CardContent>
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="underline text-sm">
-          View
-        </Link>
-      </CardFooter>
-    </Card>
+    <div className="flex w-full max-w-105 gap-x-1">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2 items-center">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="line-clamp-3 whitespace-break-spaces">
+            {ticket.content}
+          </p>
+        </CardContent>
+      </Card>
+      <div className="flex flex-col gap-y-1">{detailsButton}</div>
+    </div>
   );
 }
 
