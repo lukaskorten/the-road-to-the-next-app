76e063d96aa78b087d7ca843748c50f877db7049
Lukas Korten <mail@lukas-korten.de>
2025-12-16 14:55:15 +0100

    feat: implement createTicket function to save new tickets and revalidate ticket list

--- src/features/ticket/actions/create-ticket.ts
+++ src/features/ticket/actions/create-ticket.ts
@@ -1,11 +1,21 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
+import { ticketsPath } from '@/app/paths';
+import { prisma } from '@/lib/prisma';
+
 export async function createTicket(formData: FormData) {
   const data = {
     title: formData.get('title'),
     content: formData.get('content'),
   };
 
-  console.log(data);
-  //todo: Implement create ticket
+  await prisma.ticket.create({
+    data: {
+      title: data.title as string,
+      content: data.content as string,
+    },
+  });
+
+  revalidatePath(ticketsPath());
 }
