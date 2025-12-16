fe11699d812d0d09a820a312bfefb4c9740e0a97
Lukas Korten <mail@lukas-korten.de>
2025-12-16 10:14:23 +0100

    feat: update seed script to remove IDs and enhance logging

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,19 +2,16 @@ import { prisma } from '@/lib/prisma';
 
 const tickets = [
   {
-    id: '1',
     title: 'Ticket 1',
     content: 'This is the first ticket from the database',
     status: 'DONE' as const,
   },
   {
-    id: '2',
     title: 'Ticket 2',
     content: 'This is the second ticket from the database',
     status: 'OPEN' as const,
   },
   {
-    id: '3',
     title: 'Ticket 3',
     content: 'This is the third ticket from the database',
     status: 'IN_PROGRESS' as const,
@@ -22,9 +19,15 @@ const tickets = [
 ];
 
 async function seed() {
+  const t0 = performance.now();
+  console.log('DB Seed: Started ...');
+  await prisma.ticket.deleteMany();
+
   await prisma.ticket.createMany({
     data: tickets,
   });
+  const t1 = performance.now();
+  console.log(`DB Seed: Finished in ${(t1 - t0).toFixed(2)} ms`);
 }
 
 seed();
