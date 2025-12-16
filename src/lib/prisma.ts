0fa26b3a432c42999bb11c668f551f413bc09b66
Lukas Korten <mail@lukas-korten.de>
2025-12-16 10:25:58 +0100

    feat: refactor Prisma client initialization for improved global access

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,8 +2,18 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/generated/prisma/client';
 
-const connectionString = process.env.DIRECT_URL ?? '';
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+function createPrismaClinet() {
+  const connectionString = process.env.DIRECT_URL ?? '';
+  const adapter = new PrismaPg({ connectionString });
+  return new PrismaClient({ adapter });
+}
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || createPrismaClinet();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export { prisma };
