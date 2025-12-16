bbd21dd1c055891f79d21142a47e1d58454f5fec
Lukas Korten <mail@lukas-korten.de>
2025-12-16 12:53:14 +0100

    feat: enhance deleteTicket function to redirect after deletion

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,9 +1,13 @@
 'use server';
 
+import { redirect } from 'next/navigation';
+import { ticketsPath } from '@/app/paths';
 import { prisma } from '@/lib/prisma';
 
 export async function deleteTicket(ticketId: string) {
   await prisma.ticket.delete({
     where: { id: ticketId },
   });
+
+  redirect(ticketsPath());
 }
