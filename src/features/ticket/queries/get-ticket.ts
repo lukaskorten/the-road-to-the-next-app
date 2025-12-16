e377818c7366084e28bd99adc3d004a9217bd254
Lukas Korten <mail@lukas-korten.de>
2025-12-16 10:48:14 +0100

    feat: replace initial ticket retrieval with Prisma database query

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,8 +1,6 @@
-import { initialTickets } from '@/data';
+import { prisma } from '@/lib/prisma';
 import { Ticket } from '../types';
 
 export async function getTicket(ticketId: string): Promise<Ticket | null> {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  const ticket = initialTickets.find((t) => t.id === ticketId) || null;
-  return new Promise((resolve) => resolve(ticket));
+  return prisma.ticket.findUnique({ where: { id: ticketId } });
 }
