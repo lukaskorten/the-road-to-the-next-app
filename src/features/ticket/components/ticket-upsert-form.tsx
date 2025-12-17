87451d4863e90600137d6c2c44d2916b0a2673b2
Lukas Korten <mail@lukas-korten.de>
2025-12-17 09:20:11 +0100

    feat: refactor TicketUpsertForm to use upsertTicketAction and add loading state

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { LucideLoader } from 'lucide-react';
+import { useTransition } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -10,18 +14,26 @@ type TicketUpsertFormProps = {
 };
 
 export function TicketUpsertForm({ ticket }: TicketUpsertFormProps) {
+  const [isPending, startTransition] = useTransition();
+
+  const upsertTicketAction = (formData: FormData) => {
+    startTransition(async () => {
+      await upsertTicket.bind(null, ticket?.id)(formData);
+    });
+  };
+
   return (
-    <form
-      action={upsertTicket.bind(null, ticket?.id)}
-      className="flex flex-col gap-y-2"
-    >
+    <form action={upsertTicketAction} className="flex flex-col gap-y-2">
       <Label htmlFor="title">Title</Label>
       <Input id="text" name="title" type="text" defaultValue={ticket?.title} />
 
       <Label htmlFor="content">Content</Label>
       <Textarea id="content" name="content" defaultValue={ticket?.content} />
 
-      <Button type="submit">{ticket ? 'Update' : 'Create'}</Button>
+      <Button type="submit">
+        {isPending && <LucideLoader className="w-4 h-4 animate-spin" />}
+        {ticket ? 'Update' : 'Create'}
+      </Button>
     </form>
   );
 }
