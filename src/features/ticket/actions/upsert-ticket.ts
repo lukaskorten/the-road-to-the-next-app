9e72b90b1743b7b9447169b9dc70115b606f306b
Lukas Korten <mail@lukas-korten.de>
2025-12-17 10:37:17 +0100

    feat: add zod validation to upsertTicket function and update package dependencies

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,30 +2,40 @@
 
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
 import { ticketPath, ticketsPath } from '@/app/paths';
 import { prisma } from '@/lib/prisma';
 
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
+
 export async function upsertTicket(
   id: string | undefined,
-  actionMessage: { message: string },
+  _actionState: { message: string },
   formData: FormData
 ) {
-  const data = {
-    title: formData.get('title'),
-    content: formData.get('content'),
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    });
 
-  await prisma.ticket.upsert({
-    where: { id: id ?? '' },
-    update: {
-      title: data.title as string,
-      content: data.content as string,
-    },
-    create: {
-      title: data.title as string,
-      content: data.content as string,
-    },
-  });
+    await prisma.ticket.upsert({
+      where: { id: id ?? '' },
+      update: {
+        title: data.title as string,
+        content: data.content as string,
+      },
+      create: {
+        title: data.title as string,
+        content: data.content as string,
+      },
+    });
+  } catch {
+    return { message: 'Something went wrong' };
+  }
 
   revalidatePath(ticketsPath());
 
