245f765fcc7df7fedd632f01686d724aca7f2cf8
Lukas Korten <mail@lukas-korten.de>
2025-12-18 10:00:47 +0100

    feat: add timestamp reference in useActionFeedback to optimize effect execution

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { ActionState } from '../utils/to-action-state';
 
 type OnArgs = {
@@ -14,7 +14,15 @@ export function useActionFeedback(
   actionState: ActionState,
   options: UseActionFeedbackOptions
 ) {
+  const timestampRef = useRef(actionState.timestamp);
+
   useEffect(() => {
+    // Since the options object changes on each render (it gets re-initialized every time),
+    // a ref is used to compare the last timestamp
+    // with the current one to avoid unnecessary calls.
+
+    if (timestampRef.current === actionState.timestamp) return;
+    console.log('actionState changed:', actionState);
     if (actionState.status === 'ERROR') {
       options.onError?.({ actionState });
     }
@@ -22,5 +30,7 @@ export function useActionFeedback(
     if (actionState.status === 'SUCCESS') {
       options.onSuccess?.({ actionState });
     }
+
+    timestampRef.current = actionState.timestamp;
   }, [actionState, options]);
 }
