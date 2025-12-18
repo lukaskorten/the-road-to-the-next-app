455abe9a3fb2529dd62747739b85bf951bd28017
Lukas Korten <mail@lukas-korten.de>
2025-12-18 10:26:49 +0100

    feat: integrate Sonner for success and error notifications in TicketUpsertForm

--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useActionState } from 'react';
+import { toast } from 'sonner';
 import { FieldError } from '@/components/form/field-error';
 import { useActionFeedback } from '@/components/form/hooks/use-action-feedback';
 import { SubmitButton } from '@/components/form/submit-button';
@@ -23,10 +24,14 @@ export function TicketUpsertForm({ ticket }: TicketUpsertFormProps) {
 
   useActionFeedback(actionState, {
     onSuccess: ({ actionState }) => {
-      console.log('sucess:', actionState.message);
+      if (actionState.message) {
+        toast.success(actionState.message);
+      }
     },
     onError: ({ actionState }) => {
-      console.log('error', actionState.message);
+      if (actionState.message) {
+        toast.error(actionState.message);
+      }
     },
   });
 
