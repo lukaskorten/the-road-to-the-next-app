0ffe1bdb33e29b520378bddcf31d81d735f29eb0
Lukas Korten <mail@lukas-korten.de>
2025-12-23 06:54:31 +0100

    feat: add deadline field to seed tickets

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,28 @@
+import { formatISO } from 'date-fns';
 import { prisma } from '@/lib/prisma';
 
+const deadline = formatISO(new Date(), { representation: 'date' });
 const tickets = [
   {
     title: 'Ticket 1',
     content: 'This is the first ticket from the database',
     status: 'DONE' as const,
+    bounty: 499,
+    deadline,
   },
   {
     title: 'Ticket 2',
     content: 'This is the second ticket from the database',
     status: 'OPEN' as const,
+    deadline,
+    bounty: 699,
   },
   {
     title: 'Ticket 3',
     content: 'This is the third ticket from the database',
     status: 'IN_PROGRESS' as const,
+    deadline,
+    bounty: 899,
   },
 ];
 
