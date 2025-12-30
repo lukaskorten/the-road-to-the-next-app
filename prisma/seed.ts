1e5a0cc6089266e7176d2254bfa46aeb919385ea
Lukas Korten <mail@lukas-korten.de>
2025-12-30 06:28:07 +0100

    feat(seed): update seed script to include user creation and associate tickets with users

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,17 @@
 import { formatISO } from 'date-fns';
+import { hashPassword } from '@/features/password/utils/hash-and-verify';
 import { prisma } from '@/lib/prisma';
+
+const users = [
+  {
+    username: 'admin',
+    email: 'admin@admin.com',
+  },
+  {
+    username: 'lukas',
+    email: 'mail@lukas-korten.de',
+  },
+];
 
 const deadline = formatISO(new Date(), { representation: 'date' });
 const tickets = [
@@ -30,9 +42,14 @@ async function seed() {
   const t0 = performance.now();
   console.log('DB Seed: Started ...');
   await prisma.ticket.deleteMany();
+  await prisma.user.deleteMany();
 
+  const passwordHash = await hashPassword('geheimnis');
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({ ...user, passwordHash })),
+  });
   await prisma.ticket.createMany({
-    data: tickets,
+    data: tickets.map((ticket) => ({ ...ticket, userId: dbUsers[0].id })),
   });
   const t1 = performance.now();
   console.log(`DB Seed: Finished in ${(t1 - t0).toFixed(2)} ms`);
