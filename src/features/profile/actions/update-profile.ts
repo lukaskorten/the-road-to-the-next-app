57e873b05600471fab7b92267a45b32dd0776b1d
Lukas Korten <mail@lukas-korten.de>
2026-01-06 05:59:35 +0100

    feat(profile): enhance updateProfile function with validation and error handling

--- src/features/profile/actions/update-profile.ts
+++ src/features/profile/actions/update-profile.ts
@@ -1,9 +1,56 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import z from 'zod';
+import { accountProfilePath } from '@/app/paths';
 import {
   ActionState,
+  fromErrorToActionState,
   toErrorActionState,
+  toSuccessActionState,
 } from '@/components/form/utils/to-action-state';
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect';
+import { Prisma } from '@/generated/prisma/client';
+import { prisma } from '@/lib/prisma';
 
-export function updateProfile(actionState: ActionState, formData: FormData) {
-  // TODO: Implementation for updating the user profile
-  return toErrorActionState('not implemented');
+const updateUserSchema = z.object({
+  username: z
+    .string()
+    .min(1)
+    .max(191)
+    .refine((value) => !value.includes(' '), 'Username cannot contain spaces'),
+  firstName: z.string().min(1).max(191),
+  lastName: z.string().min(1).max(191),
+});
+
+export async function updateProfile(
+  _actionState: ActionState,
+  formData: FormData
+) {
+  const { user } = await getAuthOrRedirect();
+
+  try {
+    const data = updateUserSchema.parse({
+      ...user,
+      username: formData.get('username'),
+      firstName: formData.get('firstName'),
+      lastName: formData.get('lastName'),
+    });
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data,
+    });
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === 'P2002'
+    ) {
+      return toErrorActionState('Username is already in use', formData);
+    }
+    return fromErrorToActionState(error, formData);
+  }
+
+  revalidatePath(accountProfilePath());
+  return toSuccessActionState('Profile updated successfully');
 }
