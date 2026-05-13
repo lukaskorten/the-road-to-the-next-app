baf567f49627a8cdd730f803749d75a9a2b3adb1
Lukas Korten <mail@lukas-korten.de>
2026-05-13 06:13:42 +0200

    feat: add input OTP component for email verification form

--- src/features/auth/components/email-verification-form.tsx
+++ src/features/auth/components/email-verification-form.tsx
@@ -5,7 +5,11 @@ import { FieldError } from '@/components/form/field-error';
 import { Form } from '@/components/form/form';
 import { SubmitButton } from '@/components/form/submit-button';
 import { EMPTY_ACTION_STATE } from '@/components/form/utils/to-action-state';
-import { Input } from '@/components/ui/input';
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from '@/components/ui/input-otp';
 import { verifyEmail } from '../actions/verify-email';
 
 export function EmailVerificationForm() {
@@ -13,13 +17,23 @@ export function EmailVerificationForm() {
 
   return (
     <Form action={action} actionState={actionState}>
-      <Input
+      <InputOTP
+        maxLength={8}
         id="code"
         name="code"
-        type="text"
-        placeholder="Code"
         defaultValue={actionState.payload?.get('code') as string}
-      />
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+          <InputOTPSlot index={6} />
+          <InputOTPSlot index={7} />
+        </InputOTPGroup>
+      </InputOTP>
       <FieldError actionState={actionState} name="code" />
 
       <SubmitButton label="Verify Email" />
