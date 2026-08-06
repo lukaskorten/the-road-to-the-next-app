c2a051058881ea5265325d85a324823bab37db50
Lukas Korten <lukas.korten@extern.huk-coburg.de>
2026-08-06 05:56:06 +0200

    feat: refactor OrganizationList to use a table layout and add dropdown menu for actions

--- src/features/organizations/components/organization-list.tsx
+++ src/features/organizations/components/organization-list.tsx
@@ -1,23 +1,71 @@
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
 import { format } from 'date-fns';
+import { LucideMoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { getOrganizationsByUser } from '../queries/get-organizations-by-user';
 
 export default async function OrganizationList() {
   const organizations = await getOrganizationsByUser();
+
   return (
-    <div className="animate-fade-from-top">
-      {organizations.map((organization) => (
-        <div key={organization.id}>
-          <div>{organization.name}</div>
-          <div>
-            Joined At:{' '}
-            {format(
-              organization.membershipByUser.joinedAt,
-              'dd MMM yyyy HH:mm'
-            )}
-          </div>
-          <div>Members: {organization.membersCount}</div>
-        </div>
-      ))}
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>ID</TableHead>
+          <TableHead>Name</TableHead>
+          <TableHead>Joined At</TableHead>
+          <TableHead>Members</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {organizations.map((organization) => (
+          <TableRow key={organization.id}>
+            <TableCell>{organization.id}</TableCell>
+            <TableCell>{organization.name}</TableCell>
+            <TableCell>
+              {format(
+                organization.membershipByUser.joinedAt,
+                'dd.MM.yyyy HH:mm'
+              )}
+            </TableCell>
+            <TableCell>{organization.membersCount}</TableCell>
+            <TableCell className="text-right">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" size="icon" className="size-8">
+                    <LucideMoreHorizontal />
+                    <span className="sr-only">Open menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>Change Organization</DropdownMenuItem>
+                  <DropdownMenuItem>Show Details</DropdownMenuItem>
+                  <DropdownMenuItem>Edit Organization</DropdownMenuItem>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem variant="destructive">
+                    Delete Organization
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
