97e6db2323db9703716c94c1c7d756dee90c4dc5
Lukas Korten <lukas.korten@gmail.com>
2026-09-10 05:36:23 +0200

    refactor: simplify name display in MembershipsList component

--- src/features/organizations/components/memberships-list.tsx
+++ src/features/organizations/components/memberships-list.tsx
@@ -7,23 +7,27 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { Membership, User } from '@/generated/prisma/client';
 import { getMemberships } from '../queries/get-memberships';
 
 type MembershipsListProps = {
   organizationId: string;
 };
 
+function getName(membership: Membership & { user: User }) {
+  const { firstName, lastName, username } = membership.user;
+  return firstName && lastName ? `${firstName} ${lastName}` : username;
+}
+
 export default async function MembershipsList({
   organizationId,
 }: MembershipsListProps) {
   const memberships = await getMemberships(organizationId);
-
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead>First Name</TableHead>
-          <TableHead>Last Name</TableHead>
+          <TableHead>Name</TableHead>
           <TableHead>Joined At</TableHead>
           <TableHead className="text-right">Actions</TableHead>
         </TableRow>
@@ -34,8 +38,7 @@ export default async function MembershipsList({
 
           return (
             <TableRow key={membership.userId}>
-              <TableCell>{membership.user.firstName}</TableCell>
-              <TableCell>{membership.user.lastName}</TableCell>
+              <TableCell>{getName(membership)}</TableCell>
               <TableCell>
                 {format(membership.joinedAt, 'dd.MM.yyyy HH:mm')}
               </TableCell>
