de43f7c8a3040e8831fcae2dc97ac37036b130a2
Lukas Korten <lukas.korten@gmail.com>
2026-09-14 05:48:35 +0200

    feat: implement deleteMembership function with validation for last membership

--- src/features/memberships/actions/delete-membership.ts
+++ src/features/memberships/actions/delete-membership.ts
@@ -1,9 +1,36 @@
 'use server';
 
-import { toSuccessActionState } from '@/components/form/utils/to-action-state';
+import { revalidatePath } from 'next/cache';
+import { membershipsPath } from '@/app/paths';
+import {
+  fromErrorToActionState,
+  toErrorActionState,
+  toSuccessActionState,
+} from '@/components/form/utils/to-action-state';
+import { getAuthOrRedirect } from '@/features/auth/queries/get-auth-or-redirect';
+import { prisma } from '@/lib/prisma';
+import { getMemberships } from '../queries/get-memberships';
 
 export async function deleteMembership(userId: string, organizationId: string) {
-  // todo: delete the membership of the user and organizaation
+  await getAuthOrRedirect();
 
-  return toSuccessActionState('Membership deleted!');
+  try {
+    const { memberships } = await getMemberships(organizationId);
+    const isLastMembership = memberships.length === 1;
+
+    if (isLastMembership) {
+      return toErrorActionState(
+        "You can't delete the last membership of an organization!"
+      );
+    }
+
+    await prisma.membership.delete({
+      where: { membershipId: { userId, organizationId } },
+    });
+  } catch (error) {
+    return fromErrorToActionState(error);
+  }
+
+  revalidatePath(membershipsPath(organizationId));
+  return toSuccessActionState('The Membership has been deleted!');
 }
